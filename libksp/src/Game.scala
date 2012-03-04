1969e4911ec616704ae796bef1ae3c82052e6fa7
Ben Kelly <bkelly@uoguelph.ca>
2012-03-04 12:05:25 -0500

    Kerbol has a radius now!

--- libksp/src/Game.scala
+++ libksp/src/Game.scala
@@ -122,9 +122,9 @@ object Orbit {
   case class Body(id: Int, radius: Double, names: Set[String]) {}
 
   val bodies = Seq( // radius is in game units (meters), not km
-    Body(0,   1000.0, Set("Kerbol", "Sol", "Sun")), // Kerbol is actually an infinitely small point
-    Body(1, 600000.0, Set("Kerbin", "Kearth", "Earth")),
-    Body(2, 200000.0, Set("Mun", "Muna", "Moon"))
+    Body(0,  65.4e6, Set("Kerbol", "Sol", "Sun")),      // radius 65,400 km
+    Body(1, 600.0e3, Set("Kerbin", "Kearth", "Earth")), // radius 600km
+    Body(2, 200.0e3, Set("Mun", "Muna", "Moon"))        // radius 200km
   )
 
   def getBody(id: Int) = bodies find (_.id == id)
