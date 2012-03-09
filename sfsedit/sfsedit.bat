485adc1b93f748f0691b310349c865e5e0eb68fe
Ben Kelly <bkelly@uoguelph.ca>
2012-03-09 16:58:36 -0500

    SFSEdit launcher now checks registry to determine Java installation path
    - Fixes an issue in which people with busted Java installs that don't set $JAVA_HOME or add java.exe to $PATH couldn't run the editor

--- sfsedit/sfsedit.bat
+++ sfsedit/sfsedit.bat
@@ -1,4 +1,46 @@
 @ECHO OFF
 MODE CON: COLS=120
 TITLE KSP SFSEdit
-java -jar kessler/sfsedit.jar saves/default/persistent.sfs
+
+CALL :FindJRE
+
+:RunEditor
+"%JAVAPATH%\bin\java.exe" -jar kessler/sfsedit.jar saves/default/persistent.sfs
+
+PAUSE
+GOTO :EOF
+
+:FindJRE
+CALL :TestJRE JavaSoft\Java Runtime Environment
+CALL :TestJRE Wow6432Node\JavaSoft\Java Runtime Environment
+CALL :TestJRE JavaSoft\Java Development Kit
+CALL :TestJRE Wow6432Node\JavaSoft\Java Development Kit
+IF "%JAVAPATH%" NEQ "" GOTO :EOF
+
+ECHO Error: could not find a Java installation
+ECHO Please make sure Java is properly installed, including java.exe
+PAUSE
+GOTO :EOF
+
+:TestJRE
+SET KIT=%*
+call:ReadRegValue VER "HKLM\Software\%KIT%" "CurrentVersion"
+IF "%VER%" NEQ "" GOTO :FoundJRE
+GOTO :EOF
+
+:FoundJRE
+IF "%JAVAPATH%" NEQ "" GOTO :EOF
+call:ReadRegValue JAVAPATH "HKLM\Software\%KIT%\%VER%" "JavaHome"
+ECHO Found Java at %JAVAPATH%
+GOTO :EOF
+
+:ReadRegValue
+SET key=%2%
+SET name=%3%
+SET "%~1="
+SET reg=reg
+IF DEFINED ProgramFiles(x86) (
+  IF EXIST %WINDIR%\sysnative\reg.exe SET reg=%WINDIR%\sysnative\reg.exe
+)
+FOR /F "usebackq tokens=3* skip=1" %%A IN (`%reg% QUERY %key% /v %name% 2^>NUL`) DO SET "%~1=%%A %%B"
+GOTO :EOF
