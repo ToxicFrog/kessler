e94bde6b8117d4cdd6e83ba03bc8c81c7559651c
Ben Kelly <bkelly@uoguelph.ca>
2012-03-11 14:12:35 -0400

    Jar generation update

--- kessler/kessler_client.bat
+++ kessler/kessler_client.bat
@@ -1,4 +1,9 @@
 @ECHO OFF
 TITLE KSP Kessler Client
 
-call kessler\launcher.bat -jar kessler/client.jar
+ECHO Uploading save to server...
+CALL kessler\launcher.bat -jar kessler/client.jar put saves/default/persistent.sfs
+ECHO Downloading merged save...
+CALL kessler\launcher.bat -jar kessler/client.jar get kessler/downloaded.sfs
+COPY kessler/downloaded.sfs saves/default/persistent.sfs
+ECHO Done!
