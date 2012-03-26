e46fe02c4494fe5c1dd8b3c2a9f3786edfaccafa
Ben Kelly <bkelly@uoguelph.ca>
2012-03-26 15:50:14 -0400

    Include experimental kessler_server and kessler_client bash scripts for linux/OSX users

---
+++ kessler/kessler_client.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -ne "\033]0;KSP Kessler Client\007"
+
+cd $(dirname $0)
+echo "Uploading save to server..."
+java -cp kessler -jar kessler/client.jar put saves/default/persistent.sfs
+echo "Downloading merged save..."
+java -cp kessler -jar kessler/client.jar get saves/default/persistent.sfs
+echo "Done!"
---
+++ kesslerd/kessler_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo -ne "\033]0;KSP Kessler Server\007"
+
+cd $(dirname $0)
+java -cp kessler -jar kessler/server.jar
