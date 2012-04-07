66bd17de9e42ebbb08fea73e80acd30027c3137d
Ben Kelly <bkelly@uoguelph.ca>
2012-04-06 20:54:45 -0400

    Use new 'sync' command for client scripts

--- kessler/kessler_client.bat
+++ kessler/kessler_client.bat
@@ -1,8 +1,6 @@
 @ECHO OFF
 TITLE KSP Kessler Client
 
-ECHO Uploading save to server...
-CALL kessler\launcher.bat -jar kessler/client.jar put saves/default/persistent.sfs
-ECHO Downloading merged save...
-CALL kessler\launcher.bat -jar kessler/client.jar get saves/default/persistent.sfs
-ECHO Done!
+ECHO Synchronizing save file with server...
+CALL kessler\launcher.bat -jar kessler/client.jar sync saves/default/persistent.sfs
+echo "Done!"
--- kessler/kessler_client.sh
+++ kessler/kessler_client.sh
@@ -2,8 +2,6 @@
 echo -ne "\033]0;KSP Kessler Client\007"
 
 cd "$(dirname "$0")"
-echo "Uploading save to server..."
-java -cp kessler -jar kessler/client.jar put saves/default/persistent.sfs
-echo "Downloading merged save..."
-java -cp kessler -jar kessler/client.jar get saves/default/persistent.sfs
+echo "Synchronizing save file with server..."
+java -cp kessler -jar kessler/client.jar sync saves/default/persistent.sfs
 echo "Done!"
