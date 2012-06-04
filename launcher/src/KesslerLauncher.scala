4c80e1383564f7aa23986a47bba7ce1e0171fc32
Ben Kelly <bkelly@uoguelph.ca>
2012-06-04 19:47:30 -0400

    Project metadata update + launcher skeleton

---
+++ launcher/src/KesslerLauncher.scala
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ben
+ * Date: 14/04/12
+ * Time: 11:56 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object Launcher {
+  def main(args: Array[String]) {
+
+  }
+}
