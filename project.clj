fb0490111891cdd21e315c98e19d36803dd49a16
Ben Kelly <bk@ancilla.ca>
2013-03-29 23:27:51 -0400

    Add marginalia and point clearly dependency at clojars

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]]
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]
+            [lein-marginalia "0.7.1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clearley "0.1.0-SNAPSHOT"]
+                 [org.clojars.hozumi/clj-det-enc "1.0.0-SNAPSHOT"]]
   :git-dependencies [["https://github.com/mthvedt/clearley.git"]]
-  :source-paths ["src" ".lein-git-deps/clearley/src"]
   :main ca.ancilla.kessler.core)
