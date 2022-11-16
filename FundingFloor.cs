7b9ffbedd395b13089d8ba2e3a2c278bf7013b95
Rebecca Kelly <bk@ancilla.ca>
2022-11-15 22:08:08 -0500

    Fix budget calculation, add bonus funding per science

--- FundingFloor.cs
+++ FundingFloor.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace FundingFloor {
@@ -21,36 +22,48 @@ namespace FundingFloor {
       GameEvents.OnFundsChanged.Add(OnFundsChanged);
       GameEvents.OnReputationChanged.Add(OnReputationChanged);
       GameEvents.OnScienceChanged.Add(OnScienceChanged);
+      GameEvents.OnTechnologyResearched.Add(OnTechnologyResearched);
 
       CalcBudget();
       ClampFunds();
 
       // GameEvents.OnKSCFacilityUpgraded
       // GameEvents.OnProgressComplete
-      // GameEvents.OnTechnologyResearched
       Log("Funding Floor started.");
     }
 
     public void OnDisable() {
       Log("Shutting down Funding Floor...");
 
-      GameEvents.onGameNewStart.Remove(OnGameNewStart);
-      GameEvents.onGameStatePostLoad.Remove(OnGameStatePostLoad);
+      // GameEvents.onGameNewStart.Remove(OnGameNewStart);
+      // GameEvents.onGameStatePostLoad.Remove(OnGameStatePostLoad);
       GameEvents.OnGameSettingsApplied.Remove(OnGameSettingsApplied);
 
       GameEvents.OnFundsChanged.Remove(OnFundsChanged);
       GameEvents.OnReputationChanged.Remove(OnReputationChanged);
       GameEvents.OnScienceChanged.Remove(OnScienceChanged);
+      GameEvents.OnTechnologyResearched.Remove(OnTechnologyResearched);
 
       Log("Funding Floor shut down.");
     }
 
     int budget;
 
+    void UpdateFunding() {
+      CalcBudget(); ClampFunds();
+    }
+
+    // Budget is a linear interpolation between (MinFunding + science bonus)
+    // and MaxFunding. This also means that the science bonus counts for more
+    // when your reputation is lower.
     void CalcBudget() {
+      double actual_min = Math.Min(
+        cfg.MinFunding + science_spent * cfg.FundingPerScience,
+        cfg.MaxFunding - 1000);
       double funds_per_rep = (cfg.MaxFunding - cfg.MinFunding)/1000.0;
-      budget = (int)System.Math.Max(Reputation.CurrentRep * funds_per_rep, cfg.MinFunding);
-      Log($"Budget recalculated as {budget} with {funds_per_rep} funds/rep");
+      Log($"Calculating budget with min={cfg.MinFunding} sci={science_spent} amin={actual_min} max={cfg.MaxFunding} f/r={funds_per_rep}");
+      budget = (int)(actual_min + Reputation.CurrentRep * funds_per_rep);
+      Log($"Budget recalculated as {budget}");
     }
 
     void ClampFunds() {
@@ -63,44 +76,50 @@ namespace FundingFloor {
       Funding.Instance.SetFunds(budget, TransactionReasons.None);
     }
 
-    public void OnGameNewStart() {
-      Log($"FF:Start: funds={Funding.Instance.Funds} rep={Reputation.CurrentRep})");
-      CalcBudget();
-    }
+    // public void OnGameNewStart() {
+    //   Log($"Start: funds={Funding.Instance.Funds} rep={Reputation.CurrentRep})");
+    //   UpdateFunding();
+    // }
 
-    public void OnGameStatePostLoad(ConfigNode unused) {
-      Log($"FF:PostLoad: funds={Funding.Instance.Funds} rep={Reputation.CurrentRep})");
-      CalcBudget();
-    }
+    // public void OnGameStatePostLoad(ConfigNode unused) {
+    //   Log($"PostLoad: funds={Funding.Instance.Funds} rep={Reputation.CurrentRep})");
+    //   UpdateFunding();
+    // }
 
     public void OnGameSettingsApplied() {
-      Log($"FF:SettingsApplied: funds={Funding.Instance.Funds} rep={Reputation.CurrentRep})");
+      Log($"SettingsApplied: funds={Funding.Instance.Funds} rep={Reputation.CurrentRep})");
       cfg = HighLogic.CurrentGame.Parameters.CustomParams<Settings>();
-      CalcBudget();
-      ClampFunds();
-      // Recalc based on game settings
-      // Settings.Instance.FundingPerReputation, etc
+      UpdateFunding();
     }
 
     public void OnFundsChanged(double val, TransactionReasons txns) {
       if (Funding.Instance == null) return;
 
-      Log($"FF:OnFundsChanged: funds={Funding.Instance.Funds} new={val}, why={txns})");
-      ClampFunds();
+      Log($"OnFundsChanged: funds={Funding.Instance.Funds} new={val}, why={txns})");
+      UpdateFunding();
     }
 
     public void OnScienceChanged(float val, TransactionReasons txns) {
       if (ResearchAndDevelopment.Instance == null) return;
 
-      Log($"FF:OnScienceChanged: science={ResearchAndDevelopment.Instance.Science} new={val}, why={txns})");
+      Log($"OnScienceChanged: science={ResearchAndDevelopment.Instance.Science} new={val}, why={txns})");
     }
 
     public void OnReputationChanged(float val, TransactionReasons txns) {
       if (Reputation.Instance == null) return;
 
-      Log($"FF:OnReputationChanged: rep={Reputation.CurrentRep} new={val}, why={txns})");
-      CalcBudget();
-      ClampFunds();
+      Log($"OnReputationChanged: rep={Reputation.CurrentRep} new={val}, why={txns})");
+      UpdateFunding();
+    }
+
+    [KSPField(isPersistant = true, guiActive = false)]
+    int science_spent;
+
+    public void OnTechnologyResearched(GameEvents.HostTargetAction<RDTech, RDTech.OperationResult> action) {
+      if (action.target != RDTech.OperationResult.Successful) return;
+      science_spent += action.host.scienceCost;
+      Log($"OnTechResearched: cost={action.host.scienceCost} total={science_spent}");
+      UpdateFunding();
     }
   }
 }
--- Settings.cs
+++ Settings.cs
@@ -17,36 +17,35 @@ namespace FundingFloor {
       minValue = 500_000, maxValue = 10_000_000, stepSize = 100_00)]
     public int MaxFunding = 2_000_000;
 
-    // [GameParameters.CustomIntParameterUI("Funding per science",
-    //   toolTip = "How much each point of science spent increases your funding limit by. Unlike reputation, this cannot be lost. The upper limit depends on how much science your tech tree takes to unlock.",
-    //   minValue = 0, maxValue = 100, stepSize = 5)]
-    // public int FundingPerScience = 10;
-
-    // [GameParameters.CustomIntParameterUI("Minimum funding",
-    //   toolTip = "Funding will never be less than this, even at or below 0 reputation.",
-    //   minValue = 0, maxValue = 100000, stepSize = 1000)]
-    // public int MinFunding = 10000;
+    // Conversion ratios:
+    // Ongoing TO funds: 2500 F : 2 rep : 1 sci
+    // Ongoing FROM funds: 10k F : 2 rep : 1 sci
+    // Immediate TO funds: 100 F : 1 rep : 2 sci
+    [GameParameters.CustomIntParameterUI("Bonus funding per science",
+      toolTip = "How much min funding increases per science spent.\nOnly science spent on R&D unlocks counts.",
+      minValue = 0, maxValue = 100, stepSize = 5)]
+    public int FundingPerScience = 50;
 
     [GameParameters.CustomFloatParameterUI("Funding decay per transaction (%)",
-      toolTip = "Every purchase you make will slightly decrease reputation based on this % of the money spent.",
+      toolTip = "Spending money slightly decreases reputation.\nIf 'keep excess' is on, spending excess doesn't count.",
       minValue = 0, maxValue = 10, stepCount = 100)]
-    public float FundingDecayPercent = 30;
+    public float FundingDecayPercent = 0;
 
     [GameParameters.CustomParameterUI("Keep excess funding",
       toolTip = "Funding earned in excess of what your reputation would give you (from recovery, contracts, etc) can be spent normally, temporarily increasing your budget.")]
     public bool KeepExcessFunding = true;
 
-    [GameParameters.CustomFloatParameterUI("Excess funding conversion to science",
-      toolTip = "Excess funding earned will be converted into science at this ratio.",
-      minValue = 0, maxValue = 0.1f, stepCount = 100)]
-    public float ExcessScienceConversion = 0;
+    // [GameParameters.CustomFloatParameterUI("Excess funding conversion to science",
+    //   toolTip = "Excess funding earned will be converted into science at this ratio.",
+    //   minValue = 0, maxValue = 0.1f, stepCount = 100)]
+    // public float ExcessScienceConversion = 0;
 
     // FIXME: convert into reputation based on the amount of reputation needed to get that amount of funding,
     // rather than based on a straight conversion? I.e. if you gain 10k funds, and this ratio is set to 0.1,
     // and your reputation-to-funding ratio is set to 500, you would gain 2 reputation instead of 1000.
-    [GameParameters.CustomFloatParameterUI("Excess funding conversion to reputation",
-      toolTip = "Excess funding earned will be converted into reputation at this ratio.",
-      minValue = 0, maxValue = 0.1f, stepCount = 100)]
-    public float ExcessReputationConversion = 0;
+    // [GameParameters.CustomFloatParameterUI("Excess funding conversion to reputation",
+    //   toolTip = "Excess funding earned will be converted into reputation at this ratio.",
+    //   minValue = 0, maxValue = 0.1f, stepCount = 100)]
+    // public float ExcessReputationConversion = 0;
   }
 }
